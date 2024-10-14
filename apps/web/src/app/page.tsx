225b42db2df686828045993d07a0a1089dd4570f
Ulaş Özdemir <ozdemirrulass@gmail.com>
2024-10-14 04:40:29 +0300

    initial

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@repo/ui/components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      <main>
+        <Button>This is a shadcn ui package button</Button>
+      </main>
+    </>
+  );
+}
